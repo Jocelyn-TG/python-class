76ed56690b89c9df75431f39f5c5768fe96c15d3
Jocelyn-TG <jocelynt@lcg.unam.mx>
2024-03-22 21:08:03 -0600

    Codigo count_atgc ver 1.0

--- count_atgc/src/count_atgc.py
+++ count_atgc/src/count_atgc.py
@@ -8,13 +8,15 @@ VERSION: 3.12
 AUTHOR: JOCELYN TRUJILLO GUTIERREZ
         
 
-DESCRIPTION: Este es un proyecto que cuenta las ocurrencias de los símbolos 
-"A", "T", "G" y "C", de una secuencia de ADN que leerá a través de un archivo.
+DESCRIPTION: 
+    
+    Este es un proyecto que cuenta las ocurrencias de los símbolos "A", "T", 
+    "G" y "C", de una secuencia de ADN que leerá a través de un archivo.
         
+CATEGORY:
 
-CATEGORY
+    Análisis de secuencias de ADN/Bioinformática
         
-
 USAGE
 
     % python programName
@@ -22,54 +24,43 @@ USAGE
 
 ARGUMENTS
 
+    archivo(str): Archivo que contiene la cadena para realizar el conteo.
+
 
 METHOD
 
-
-SEE ALSO
-
-
-        
+    El método utilizado en el código anterior es un enfoque simple para contar 
+    los nucleótidos (A, C, G, T) en una secuencia de ADN contenida en un archivo 
+    de texto. A continuación, describiré brevemente cada parte del método:
+        1.- Obtención del nombre del archivo.
+        2.- Apertura del archivo.
+        3.- Lectura de la secuencia de ADN.
+        4.- Conteo de nucleótidos.
+        5.- Impresión de resultados.
+     
 '''
-
-
-# ===========================================================================
-# =                            imports
-# ===========================================================================
-
-
-
-
-
-# ===========================================================================
-# =                            Command Line Options
-# ===========================================================================
-
-
-
-
-
-
-# ===========================================================================
-# =                            functions
-# ===========================================================================
-
-
-
 
 # ===========================================================================
 # =                            main
 # ===========================================================================
 
 
-# Open file
-with open ("archivo.txt") as arch:
-    secuencia = arch.readline
+# Obtención del archivo
+archivo = input("Por favor, introduce el nombre del archivo que contiene la secuencia de ADN: ")
 
-# Count ATGC in file
+# Abre el archivo
+try:
+    with open(archivo, "r") as arch:
+        secuencia = arch.read().upper()        
+except FileNotFoundError:
+    print(f"Error: No se encontró el archivo '{archivo}'.")
 
+# Hace el conteo 
+conteo = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+for nucleotido in secuencia:
+    conteo[nucleotido] += 1 
 
-# Print Results
-
-
+# Imprime los resultados
+for nucleotido, total in conteo.items():
+    print(f"{nucleotido}: {total}")
 
